2dec39e1b3972ac5345f9467b9c9053166086ec6
Morten Lied Johansen <mortenjo@ifi.uio.no>
2021-04-27 21:16:37 +0200

    Remove Teams, add gcloud auth success to auto-close script

--- auto_close.user.js
+++ auto_close.user.js
@@ -1,13 +1,13 @@
 // ==UserScript==
 // @name         Auto-close tab!
 // @namespace    https://github.com/mortenlj/userscripts
-// @version      0.3
+// @version      0.4
 // @downloadURL  https://raw.githubusercontent.com/mortenlj/userscripts/master/auto_close.user.js
 // @description  Fade out and close the tab automatically
 // @author       Morten Lied Johansen
-// @match        https://teams.microsoft.com/dl/launcher/launcher.html*
 // @match        https://*.slack.com/ssb/signin_redirect*
 // @match        https://*.slack.com/archives/
+// @match        https://cloud.google.com/sdk/auth_success
 // @grant        window.close
 // ==/UserScript==
 
