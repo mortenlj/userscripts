a3f5d5a36c4b95cd8437c1ff1cfb185f64f9b5da
Morten Lied Johansen <mortenjo@ifi.uio.no>
2021-08-03 15:18:41 +0200

    Add auto-close of microsoft application verify

--- auto_close.user.js
+++ auto_close.user.js
@@ -1,13 +1,14 @@
 // ==UserScript==
 // @name         Auto-close tab!
 // @namespace    https://github.com/mortenlj/userscripts
-// @version      0.5.1
+// @version      0.5.2
 // @downloadURL  https://raw.githubusercontent.com/mortenlj/userscripts/master/auto_close.user.js
 // @description  Fade out and close the tab automatically
 // @author       Morten Lied Johansen
 // @match        https://*.slack.com/ssb/signin_redirect*
 // @match        https://*.slack.com/archives/
 // @match        https://cloud.google.com/sdk/auth_success
+// @match        https://login.microsoftonline.com/appverify
 // @match        http://localhost/?code=*
 // @match        https://*.zoom.us/j/*
 // @match        https://zoom.us/j/*
