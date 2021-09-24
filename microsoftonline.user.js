c1fe695214b3d866729fdf7be47d4fb3ca666e3c
Morten Lied Johansen <mortenjo@ifi.uio.no>
2021-09-24 10:20:16 +0200

    Fix URLs

--- microsoftonline.user.js
+++ microsoftonline.user.js
@@ -1,12 +1,12 @@
 // ==UserScript==
 // @name         Microsoft online auto flow
 // @namespace    https://github.com/mortenlj/userscripts
-// @version      0.1
+// @version      0.1.1
 // @downloadURL  https://raw.githubusercontent.com/mortenlj/userscripts/master/microsoftonline.user.js
 // @description  Automate giving access to application
 // @author       Morten Lied Johansen
-// @match        https://login.microsoftonline.com/common/oauth2/deviceauth -- select account
-// @match        https://login.microsoftonline.com/62366534-1ec3-4962-8869-9b5535279d0b/reprocess?ctx=rQIIARXPv3OTUADA8ZCfbTyfvV7dvHPJpPcUHvCA3jk8IL9JkZhEkyVH4EGqoRAKwbi4Ojiok7OLV0f_gOrs4HXVidHroou7dfuOn--dEnePPWyI7kJQ5AWCC86ToSC7IpRtzEPKCpRnsSMgG8X79b3zgx_n-7sT8-Pb1zdqX9RbFwzzi2F-FvdMkiZLpNPNsUP_52URnNIVdZK57ThhepJ8KjVslkou5hSIHA9BAfMctLHHQsHjJOoqoseJ_OdSAyMeY5EXIEcdHgoKRlCWsQKVhSjyIpIUl11clIuX5SK786FyhardZG4P_tbbZ-_P0MvftcK3yn1tHC5VqhizvjggD5MYWVwq0Kl5fJf1MY6iDj45Rag1kbH8gD3k3lT1P1XmVa3w9XrhHfjOEIuohDQJITqEukH4iSlZsev6PV96TDfx0WY1bGb81BnjdqZqTjvupJMgeBRw80AiPatjbfhMz56MoGiTZ82hSbTpVibRVrMgaaqzznyYjDKN9INWoD4frLuc5PBcn6ZD2d2Go-6RMV62lTAOomxqiGuoRC0kxrPAear2iLH2tsiS3STOpBdXa-Oe6huJkkZhV3LjlR-EWNkG-srUAldr-oTkgMlBMQelHJRzUMlBNQe1HOzkYDcH9Rxc-wc1&sessionid=1c0028ca-223d-4c60-88cd-36edefa4166e -- "Prøver du å logge på kubectl"
+// @match        https://login.microsoftonline.com/common/oauth2/deviceauth
+// @match        https://login.microsoftonline.com/*/reprocess*
 // ==/UserScript==
 
 (function() {
