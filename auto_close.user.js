6fd2a882155d36277e6675c5afc39a046e62b7e7
Morten Lied Johansen <mortenjo@ifi.uio.no>
2022-08-18 12:59:58 +0200

    Forgot to bump version on last change

--- auto_close.user.js
+++ auto_close.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Auto-close tab!
 // @namespace    https://github.com/mortenlj/userscripts
-// @version      0.5.2
+// @version      0.5.3
 // @downloadURL  https://raw.githubusercontent.com/mortenlj/userscripts/master/auto_close.user.js
 // @description  Fade out and close the tab automatically
 // @author       Morten Lied Johansen
